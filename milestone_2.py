4806722e866add725fe733daeec69b0a9bfc10fb
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-25 12:35:18 +0100

    adds request for user input of a single letter. While input string length is not equal to one letter, it throws an error to the user and requests a new input. Input is stored in variable "guess".

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,13 @@ print(word_list)
 word = random.choice(word_list)
 
 print(word)
+
+
+guess=input('please enter a single letter')
+
+while len(guess) != 1:
+    print('hey! I said a single letter!')
+    guess=input('please enter a single letter')
+
+print('thanks!')
+
