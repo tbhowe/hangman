7af67058cd471d8b01cd3753ba3fbc482327bdcc
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-25 13:30:58 +0100

    This revision changes the original while loop (which relied on a flag to escape) to a new while loop based around while(true), escaped using the break command.

--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,12 @@
-loop_var=1
 
-while loop_var==1:
+while (True):
     # Request input from user of a single letter
     guess=input("please enter a single letter")
 
     #checks that input is alphabetical AND of length 1. Escapes if both conditions met
     if len(guess)==1 and guess.isalpha() == 1:
         print('thanks!')
-        loop_var=0
+        break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
