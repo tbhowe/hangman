59019ea007cb94a139cc1c737802c9552f555bc5
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-25 12:45:50 +0100

    Adds an if statement to check that guess is alphabetical

--- milestone_2.py
+++ milestone_2.py
@@ -1,19 +1,27 @@
 # import relevant modules
 import random
 
+# Create word list
 word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
 print(word_list)
 
+# Choose a random word from the list of possible words
 word = random.choice(word_list)
-
 print(word)
 
+# Request input from user of a single letter
+guess=input("please enter a single letter")
 
-guess=input('please enter a single letter')
-
+# while loop to ensure only a single element is input for the guess
 while len(guess) != 1:
-    print('hey! I said a single letter!')
-    guess=input('please enter a single letter')
+    print("hey! I said a single letter!")
+    guess=input("please enter a single letter")
 
 print('thanks!')
+
+# Checkl that the guess is an alphabetical
+if string.isalpha==1:
+    print("good guess!")
+else:
+    print("Oops! That is not a valid input.")
 
