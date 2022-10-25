733f032f6c668343e16c417d0c1c9a9933de6dea
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-25 18:43:49 +0100

    adds an if loop to check if guessed letter is in the target word. Returns appropriate user feedback in either case"

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,13 @@
+# import relevant modules
+import random
+
+# Create word list
+word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
+
+
+# Choose a random word from the list of possible words
+word = random.choice(word_list)
+
 
 while True:
     # Request input from user of a single letter
@@ -9,4 +19,13 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+# Check whether guess is in word
+
+if guess in word:
+    print("Good guess! " + guess + " is in the word.")
+else:
+    print("Sorry, " + guess + " is not in the word. Try again.")
+
+
 
