4a0f20adcaec5856b4832577d7eb3703e2e07b58
tbhowe <53109226+tbhowe@users.noreply.github.com>
2022-10-25 16:02:53 +0100

    deleted whitespace line in line 5

--- milestone_3.py
+++ milestone_3.py
@@ -2,7 +2,6 @@
 while True:
     # Request input from user of a single letter
     guess=input("please enter a single letter")
-
     #checks that input is alphabetical AND of length 1. Escapes if both conditions met
     if len(guess)==1 and guess.isalpha() == 1:
         print('thanks!')
