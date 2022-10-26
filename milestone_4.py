298b75c4022e86f7a906813b6119aa1426eb53bc
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-26 13:56:43 +0100

    adds the functions from milestone_3.py as methods in the class Hangman, with the small modification that the if loop in ask_for_input now excludes incorrectly formatted guesses in the if statement, plus checks if a guess has been previously made in an elif statement, before calling check guess in the else statement.

--- milestone_4.py
+++ milestone_4.py
@@ -11,10 +11,37 @@ class Hangman:
         self.word_guessed= ['_'] * len(self.word)
         self.num_letters=len(list(set(self.word)))
         self.list_of_guesses=[]
+    
+    def check_guess(self,guess):
+    # This function checks if the guessed letter is in the word
+        guess= guess.lower()
+        if guess in self.word:
+            print("Good guess! " + guess + " is in the word.")
+            return()
+        else:
+            print("Sorry, " + guess + " is not in the word. Try again.")
+        return()
 
+    def ask_for_input(self):
+        #This function requests an input, returns the input if correctly formatted, 
+        # else asks for a new guess.
+        while True:
+        # Request input from user of a single letter
+            guess=input("please enter a single letter")
+        #checks that input is alphabetical AND of length 1. Escapes if both conditions met
+            if len(guess)!=1 or guess.isalpha() != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses+=guess
+                
 
-# # Create word list
-# word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
+# Create word list
+word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
 
-# # Create instance of Hangman Class, passing word list to it.
-# test_instance=Hangman(word_list,5)
+# Create instance of Hangman Class, passing word list to it.
+test_instance=Hangman(word_list,5)
+
+test_instance.ask_for_input()
