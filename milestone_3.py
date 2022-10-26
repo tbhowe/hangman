a1344124d20bbb5176cfc70fb3e9f74f9f7698c7
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-26 11:53:12 +0100

    updates milestone_3.py, defining functions to ask for input and check the guess. The relevant code blocks from the previous version are moved inside the functions. Function ask_for_input now calls function check_guess, and the script itself now consists only of the word list, the statement that chooses the random word choice, and a call to the functio ask_for_input

--- milestone_3.py
+++ milestone_3.py
@@ -1,30 +1,46 @@
 # import relevant modules
 import random
 
+# define functions for use in the Hangman script
+
+def check_guess(guess):
+    # This function checks if the guessed letter is in the word
+    guess= guess.lower()
+    if guess in word:
+        print("Good guess! " + guess + " is in the word.")
+        return()
+    else:
+        print("Sorry, " + guess + " is not in the word. Try again.")
+        return()
+
+def ask_for_input():
+    #This function requests an input, returns the input if correctly formatted, 
+    # else asks for a new guess.
+    while True:
+        # Request input from user of a single letter
+        guess=input("please enter a single letter")
+        #checks that input is alphabetical AND of length 1. Escapes if both conditions met
+        if len(guess)==1 and guess.isalpha() == 1:
+            print('thanks!')
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    #Checks whether guess is in the word
+    check_guess(guess)
+    #returns guess
+    return(guess)
+
+
+
+
+# Start of main Hangman script	
+
 # Create word list
 word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
-
 
 # Choose a random word from the list of possible words
 word = random.choice(word_list)
 
-
-while True:
-    # Request input from user of a single letter
-    guess=input("please enter a single letter")
-    #checks that input is alphabetical AND of length 1. Escapes if both conditions met
-    if len(guess)==1 and guess.isalpha() == 1:
-        print('thanks!')
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-# Check whether guess is in word
-
-if guess in word:
-    print("Good guess! " + guess + " is in the word.")
-else:
-    print("Sorry, " + guess + " is not in the word. Try again.")
-
+ask_for_input()
 
 
