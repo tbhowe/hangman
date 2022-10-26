af0c84d698b56a267f4c292fc58acc6c4cf832bb
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-26 16:47:35 +0100

    Adds milestone_5.py, a script for a fully functional version of the hangman gam, albeit in extremely basic form. The script defines a function play_game, which instantiates an object of the class Hangman, and then orchestrates the logic of the game inside an iterative loop. The loop is escaped by either winning the game (number of letters remaining reaches zero, or by losing (number of lives reaches zero).

---
+++ milestone_5.py
@@ -0,0 +1,68 @@
+# import relevant modules
+import random
+
+# define the Hangman Class:
+class Hangman:
+    
+    def __init__(self,word_list,num_lives):
+        self.word_list=word_list
+        self.num_lives=5
+        self.word = random.choice(word_list)
+        self.word_guessed= ['_'] * len(self.word)
+        self.num_letters=len(list(set(self.word)))
+        self.list_of_guesses=[]
+    
+    def check_guess(self,guess):
+    # This function checks if the guessed letter is in the word
+        guess= guess.lower()
+        if guess in self.word:
+            print("Good guess! " + guess + " is in the word.")
+            for iLetter in range(len(self.word)):
+                if self.word[iLetter]==guess:
+                    self.word_guessed[iLetter]=guess
+            self.num_letters-=1   
+            return()
+        else:
+            self.num_lives-=1
+            print("Sorry, " + guess + " is not in the word.")
+            print("You have " + str(self.num_lives) + " lives left.")
+        #self.list_of_guesses+=guess
+        return()
+
+    def ask_for_input(self):
+        #This function requests an input, returns the input if correctly formatted, 
+        # else asks for a new guess.
+        while True:
+        # Request input from user of a single letter
+            guess=input("please enter a single letter")
+        #checks that input is alphabetical AND of length 1. Escapes if both conditions met
+            if len(guess)!=1 or guess.isalpha() != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+                
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses+=guess  # This is a duplicate of functionality in check_guess method now
+                break
+                
+
+# Create word list
+word_list = ["lingon", "raspberry", "cherry","rambutan","guava"]
+
+# Define play_game function
+
+def play_game(word_list):
+    game=Hangman(word_list,5)
+    while True:
+        if game.num_lives==0:
+            print("You lost!")
+            return()
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print("Congratulation. You won the game!")
+            return()
+
+play_game(word_list)
