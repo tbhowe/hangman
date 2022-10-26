b0304489282a58c21a6c50b0b8a4421c2cfe9be6
Tim Howe <rrritalin@Tims-Air.lan>
2022-10-26 15:54:55 +0100

    Fixes reporting on incorrect guess. Additionally comments out duplicate code in ask_for_input method that adds guess to list of checked guesses. This is now part of the functionality of the check_guess method.

--- milestone_4.py
+++ milestone_4.py
@@ -25,6 +25,7 @@ class Hangman:
         else:
             self.num_lives-=1
             print("Sorry, " + guess + " is not in the word. Try again.","You have {num_lives} lives left.")
+        self.list_of_guesses+=guess
         return()
 
     def ask_for_input(self):
@@ -42,7 +43,7 @@ class Hangman:
                 
             else:
                 self.check_guess(guess)
-                self.list_of_guesses+=guess
+                #self.list_of_guesses+=guess  # This is a duplicate of functionality in check_guess method now
                 break
                 
 
